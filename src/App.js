8fac81ca5693fda6642bd3a523a71ee7c8251676
yi chiang <yi@yis-mbp-2.lan>
2020-03-02 08:03:07 -0800

    added previous file back

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import './App.scss';
+import Example from './physician-empower'
+
+import React, { Component } from 'react'
+import Userexperiences from './listbox';
+import Button from '@bit/semantic-org.semantic-ui-react.button'
+
+import DisplayQrcode from '@bit/globalinput.web.display-qrcode';
+import DragAndDrop from '@bit/bronz3beard.react-component-collection.drag-and-drop';
+
+import ToDoList from '@bit/learn-bit.react-demo-app.to-do-list';
+import Timer from './Timer'
+import AtomSpinner from '@bit/bondz.react-epic-spinners.atom-spinner';
+
+
+// import './font.css';
+
+
+
+const style = <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css'/>
+
+const ButtonExampleGroupIconSize = () => (
+  <Button.Group basic size='small'>
+    <Button icon='file' />
+    <Button icon='save' />
+    <Button icon='upload' />
+    <Button icon='download' />
+  </Button.Group>
+)
+
+const handleDrop = (files) => {
+	let tempFileList = [];
+
+	for (let i = 0; i < files.length; i++) {
+		const file = files[i];
+		if (!file.name) {
+			return;
+		}
+		alert(file.name)
+		tempFileList.push(file);
+	}
+};
+
+
+export class App extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <p className="Company-name">
+          <div className="Atoms">
+              <AtomSpinner
+              color='#000000'
+              size='100'
+              />
+            </div>
+            Nᵀᴴ OPINION
+          </p>
+        <div className='question'>What is best anticoagulant for atrial fibrillation?</div>
+        <div className='Checkbox-container'>
+          <div className = 'Test' style={{width: '150vw'}}>
+            <ToDoList/>
+            <Timer></Timer>
+
+          </div>
+        </div>
+        
+        {/* <Userexperiences></Userexperiences>      */}
+        <div className='toolbar'>{style}<ButtonExampleGroupIconSize/></div>
+        <div className='QR-container'>
+          <div className='QR-format'>
+ 
+            
+          </div> 
+        </div>
+        <DragAndDrop handleDrop={handleDrop}>
+          <div
+            type="file"
+            name="files"
+            accept="image/*"
+            multiple
+            style={{ height: `${300}px`, width: `${500}px` }}
+          ></div>
+        </DragAndDrop>
+        
+      </div>
+      
+    )
+  }
+}
+
+export default App
