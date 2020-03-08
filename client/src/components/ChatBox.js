01b6c3db9c72fcd612feef756555a8ba0c48d408
yi chiang <yi@yis-mbp-2.lan>
2020-03-08 14:13:11 -0700

    update floating button

---
+++ client/src/components/ChatBox.js
@@ -0,0 +1,51 @@
+import React, { Component, Fragment } from "react";
+import styles from "../styles/FloatingMenu.module.css";
+import Iframe from './Iframe'
+import { List, Button, Icon, Message, Sticky, Header, Image } from 'semantic-ui-react'
+
+class ChatBox extends Component {
+  constructor() {
+  		super();
+
+
+  	}
+
+
+
+  	render() {
+
+  		return(
+        <Fragment>
+<div className={styles.overlay}></div>
+           <div className={styles.chatbotPanel}>
+           <Header as='h2' className={styles.customMessageHead}
+           attached>
+             <Icon name='heartbeat' />
+             <Header.Content>
+               Janet Assistant
+               <Header.Subheader>Let's chat</Header.Subheader>
+             </Header.Content>
+           </Header>
+
+
+             <div className='attached fluid segment'>
+             <Iframe
+             src={'https://webchat.botframework.com/embed/ntozwu-qna-wellspring-bot?s=inVtGkA7vCM.w7KrGgKpZeqVW9HhSX8KcdjJD6sNOAvOP_EIeUiC5g4'}
+             style="border: 0; overflow: auto;"
+             width={'100%'} height={'600px'}></Iframe>
+             </div>
+             {/*
+               <Message attached='bottom' warning>
+               <Icon name='help' />
+               Already signed up?&nbsp;<a href='#'>Login here</a>&nbsp;instead.
+             </Message>
+             */}
+           </div>
+           </Fragment>
+
+
+);
+  	}
+}
+
+export default ChatBox;
--- client/src/components/FloatingMenu.js
+++ client/src/components/FloatingMenu.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
-import "../styles/FloatingMenu.css";
+import React, { Component, Fragment } from "react";
+import styles from "../styles/FloatingMenu.module.css";
 import Iframe from './Iframe'
+import ChatBox from './ChatBox'
 import { List, Button, Icon, Message, Sticky, Header, Image } from 'semantic-ui-react'
 
 class FloatingMenu extends Component {
@@ -19,37 +20,17 @@ class FloatingMenu extends Component {
   	render() {
 
   		return(
-  			 <div className='floating-menu'>
-         {this.state.toggled &&
-
-           <div className='chat-bot-panel'>
-           <Header as='h2' className='custom-message-head'
-           attached>
-             <Icon name='heartbeat' />
-             <Header.Content>
-               Here is Jenna
-               <Header.Subheader>Let's chat</Header.Subheader>
-             </Header.Content>
-           </Header>
+        <Fragment>
 
 
-             <div className='attached fluid segment'>
-             <Iframe
-             src={'https://webchat.botframework.com/embed/ntozwu-qna-wellspring-bot?s=inVtGkA7vCM.w7KrGgKpZeqVW9HhSX8KcdjJD6sNOAvOP_EIeUiC5g4'}
-             width={'100%'} height={'500px'}></Iframe>
-             </div>
-             {/*
-               <Message attached='bottom' warning>
-               <Icon name='help' />
-               Already signed up?&nbsp;<a href='#'>Login here</a>&nbsp;instead.
-             </Message>
-             */}
-           </div>
 
-          }
-            <Icon circular className='floating-menu-icon' inverted color='teal' name='rocketchat' onClick={this.toggleMenu.bind(this)} />
+          <div className={[styles.floatingMenuButton].join(' ')}>
+          {this.state.toggled && <ChatBox/>}
 
-  		    </div>
+          <Icon circular className={styles.floatingMenuIcon} inverted color='teal' name='rocketchat' onClick={this.toggleMenu.bind(this)} />
+          </div>
+          </Fragment>
+
 );
   	}
 }
