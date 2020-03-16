db2e215a78d629efb8cf0735eeb853c80b8bddae
Jay Nair <jay.sethuraj@gmail.com>
2020-03-15 23:35:42 -0700

    remove config values from git

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.endpoint = 'https://nthopinion-db.documents.azure.com:443/'
-config.key = '4smin1XutXuPfpdTl25pwPDLJSxvS2ulrCfRqidNkee58ke44FMu85AMXFraxRb2hUPHH2SjEqm2FsPbPghVLw=='
+config.endpoint = ''
+config.key = ''
 
 // config.database = {
 //   id: 'QuestionList'
@@ -10,6 +10,6 @@ config.key = '4smin1XutXuPfpdTl25pwPDLJSxvS2ulrCfRqidNkee58ke44FMu85AMXFraxRb2hU
 // config.container = {
 //   id: 'DOCUMENTCOLLECTIONID'
 // }
-config.databaseId = 'QuestionList'
-config.containerId = 'Items'
+config.databaseId = ''
+config.containerId = ''
 module.exports = config
