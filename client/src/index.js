40cf337057436d5cb25c8dba39e63a5711ecf6be
MalaikaAbid <malaika11@outlook.com>
2020-03-18 11:59:33 -0600

    moved buttons to sidebar menu format

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import { Grid,List, Button } from 'semantic-ui-react'
+import { Grid,List, Button, Icon } from 'semantic-ui-react'
 import App from './containers/App'
 import About from './components/About'
 import PhysicianView from './components/PhysicianView'
@@ -12,6 +12,7 @@ import thunk from 'redux-thunk'
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import AuthProvider from './AuthProvider'
 import styles from './styles/NavLink.css'
+// import SidebarExampleSidebar from './components/NavLink'
 
 const store = createStore(
   rootReducer,
@@ -22,21 +23,23 @@ const store = createStore(
 render(
   <Provider store={store}>
     <Router>
-    <div className = {styles.NavLinks} id="nav">
+
+   
+    <div className = {styles.NavLink} class="ui visible left demo vertical inverted sidebar labeled icon menu">
       <Grid rows={3}>
         
         <Grid.Column>
 
           <Grid.Row>
-          <Button floated="right"  color='teal' href="https://nonprofit.covid19webapp.com/about/#lp-pom-text-104"> About </Button>
+          <Button floated="right"  color='teal' href="https://nonprofit.covid19webapp.com/about/#lp-pom-text-104" target="_blank"> About </Button>
           </Grid.Row>
 
           <Grid.Row>
-          <Button floated="right"  color='teal' href=" https://nonprofit.covid19webapp.com/about/#lp-pom-text-90"> Are you a physcian? </Button>
+          <Button floated="right"  color='teal' href=" https://nonprofit.covid19webapp.com/about/#lp-pom-text-90" target="_blank"> Are you a physcian? </Button>
           </Grid.Row>
           
           <Grid.Row>
-          <Button floated="right"  color='teal' href=" https://nonprofit.covid19webapp.com/about/#lp-pom-text-26 "> Are you a developer? </Button>
+          <Button floated="right"  color='teal' href=" https://nonprofit.covid19webapp.com/about/#lp-pom-text-26" target="_blank"> Are you a developer? </Button>
           </Grid.Row>
           
         </Grid.Column> 
