9ab33080a8baf56808e2a2a8a7ffca181bc0d09c
MalaikaAbid <malaika11@outlook.com>
2020-03-17 18:04:14 -0600

    Adding outbound link buttons

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import { Grid,List, Button } from 'semantic-ui-react'
+
 import { Provider } from 'react-redux'
 import App from './containers/App'
 import About from './components/About'
@@ -8,8 +10,9 @@ import PhysicianView from './components/PhysicianView'
 import * as serviceWorker from './serviceWorker'
 import rootReducer from './reducers'
 import thunk from 'redux-thunk'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import AuthProvider from './AuthProvider'
+import styles from './styles/NavLink.css'
 
 const store = createStore(
   rootReducer,
@@ -20,6 +23,25 @@ const store = createStore(
 render(
   <Provider store={store}>
     <Router>
+    <div className = {styles.NavLinks} id="nav">
+          <Grid rows={3}>
+            <Grid.Column>
+              <Grid.Row>
+              <Button basic color='teal' href="https://nonprofit.covid19webapp.com/about"> About </Button>
+              </Grid.Row>
+
+              <Grid.Row>
+              <Button basic color='teal' href="https://nonprofit.covid19webapp.com/about"> Are you a physcian? </Button>
+              </Grid.Row>
+              
+              <Grid.Row>
+              <Button basic color='teal' href="https://nonprofit.covid19webapp.com/about"> Are you a developer? </Button>
+              </Grid.Row>
+              
+            </Grid.Column> 
+
+          </Grid>
+     </div>     
       <Route exact path='/physician' component={AuthProvider(PhysicianView)} />
       <Route exact path='/physician-public' component={PhysicianView} />
       <Route exact path='/about' component={About} />
