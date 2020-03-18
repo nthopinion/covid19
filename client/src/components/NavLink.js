b612606063df4b987578ac48acaf44f254cf8043
MalaikaAbid <malaika11@outlook.com>
2020-03-18 13:59:20 -0600

    slide out sidebar menu

--- client/src/components/NavLink.js
+++ client/src/components/NavLink.js
@@ -1,13 +1,76 @@
-// import { useBooleanKnob } from '@stardust-ui/docs-components'
-// import React from 'react'
-// import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Grid,List, Button, Icon } from 'semantic-ui-react'
 
-// export default class MyComponent extends Component {
-//     componentDidMount() {
-//       const script = document.createElement("script");
-  
-//       script.src = "js/myScript.js";
-//       script.async = true;
-  
-//       document.body.appendChild(script);
-//     }
+import styles from '../styles/NavLink.css'
+
+class Menu extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+          showMenu: false,
+        };
+        
+        this.showMenu = this.showMenu.bind(this);
+        this.closeMenu = this.closeMenu.bind(this);
+      }
+      
+      showMenu(event) {
+        event.preventDefault();
+        
+        this.setState({ showMenu: true }, () => {
+          document.addEventListener('click', this.closeMenu);
+        });
+      }
+      
+      closeMenu() {
+        this.setState({ showMenu: false }, () => {
+          document.removeEventListener('click', this.closeMenu);
+        });
+      }
+    
+  render() {
+    return (
+      
+        <div>
+        <Button class="ui labeled icon button" onClick={this.showMenu}> <i class="bars icon" onClick={this.showMenu}></i>  
+      </Button>
+      
+      {
+          this.state.showMenu
+            ? (
+                <div className="menu">
+                <div className = {styles.NavLink} class="ui visible left demo vertical inverted sidebar labeled icon menu">
+                    <Grid rows={3}>
+                        
+                        <Grid.Column>
+        
+                        <Grid.Row>
+                        <Button floated="right"  color='teal' href="https://nonprofit.covid19webapp.com/about/#lp-pom-text-104" target="_blank"> About </Button>
+                        </Grid.Row>
+        
+                        <Grid.Row>
+                        <Button floated="right"  color='teal' href=" https://nonprofit.covid19webapp.com/about/#lp-pom-text-90" target="_blank"> Are you a physcian? </Button>
+                        </Grid.Row>
+                        
+                        <Grid.Row>
+                        <Button floated="right"  color='teal' href=" https://nonprofit.covid19webapp.com/about/#lp-pom-text-26" target="_blank"> Are you a developer? </Button>
+                        </Grid.Row>
+                        
+                        </Grid.Column> 
+        
+                    </Grid>
+                </div>
+                </div>
+            )
+            : (
+              null
+            )
+        }
+        
+      </div>
+    );
+  }
+}
+
+export default Menu;
