5e0f016e7584502bed037d8f8da9ea5ffa740063
Joslin Chan <joslin.chan@unbounce.com>
2020-03-20 15:36:46 -0700

    Hook up submit and styling

--- client/src/containers/AddQuestionForm.js
+++ client/src/containers/AddQuestionForm.js
@@ -1,18 +1,17 @@
 import React, { Component, createRef } from 'react'
-import { Button, Form, Modal } from 'semantic-ui-react'
+import { Button, Form, Modal, ModalActions } from 'semantic-ui-react'
 import _ from 'lodash'
 import { connect } from 'react-redux'
 import { postQuestion } from '../actions'
-import '../styles/PatientBoard.css'
-
-const AskQuestionButton = (
-  <Button className='ask-button'>Ask a question</Button>
-)
+import '../styles/AskQuestionForm.css'
 
 class AddQuestionForm extends Component {
   state={}
   constructor(props) {
     super(props)
+    this.state = {
+      showModal: false
+    }
   }
 
   componentDidMount() {
@@ -26,27 +25,35 @@ class AddQuestionForm extends Component {
   }
   handleChange = (e, { value }) => this.setState({ value })
 
+  closeModal = () => {
+    this.setState({ showModal: false});
+  }
+  openModal = () => {
+    this.setState({ showModal: true});
+  }
+
   render() {
+    const { showModal } = this.state;
+    const AskQuestionButton = (
+      <Button onClick={this.openModal} className='ask-button'>Ask a question</Button>
+    )
 
     return (
-/*       <Form onSubmit={this.handleSubmit}>
-           <Form.TextArea
-           value={this.state.value}
-           onChange={this.handleChange}
-           label='Do you have a question?'
-           placeholder='Tell us more about it...'
-
-           />
-           <Form.Button type='submit'>Submit</Form.Button>
-         </Form> */
-         <Modal trigger={AskQuestionButton}>
+         <Modal trigger={AskQuestionButton} closeIcon onClose={this.closeModal} open={showModal}>
          <Modal.Header>New Question</Modal.Header>
          <Modal.Content>
            <Modal.Description>
-             <Form>
-               <Form.Input fluid label='Title' />
-               <Form.TextArea label='Description' />
-               <Form.Button type='submit' color='#006EAA'>Submit</Form.Button>
+             <Form onSubmit={this.handleSubmit}>
+                <Form.Input fluid label='Title' />
+                <Form.TextArea 
+                  label='Description' 
+                  value={this.state.value} 
+                  onChange={this.handleChange}
+                />
+                <div className='flex-container'>
+                    <Form.Button type='submit'>Submit</Form.Button>
+                    <Button onClick={this.closeModal}>Cancel</Button>
+                </div>
              </Form>
            </Modal.Description>
          </Modal.Content>
