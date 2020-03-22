699e5aa61368dba4faafc0b06ef1e58bccc26ec6
Aviskar KC <aviskarkc10@gmail.com>
2020-03-22 23:42:38 +0545

    Add pusher variables to example config

--- api/config.example.js
+++ api/config.example.js
@@ -1,8 +1,6 @@
 var config = {}
-
-config.endpoint = ''
-config.key = ''
-
+config.endpoint = 'https://ntozwu-ts-covid19-db.documents.azure.com:443/'
+config.key = '6o3oXBpwQN6God86QTXd67FoFa35jKmMPfhbxfHyuTsogQ6C46p75EC6e31jsKnujYztCZjpYC5PwBe2eC7ioA=='
 // config.database = {
 //   id: 'QuestionList'
 // }
@@ -10,6 +8,11 @@ config.key = ''
 // config.container = {
 //   id: 'DOCUMENTCOLLECTIONID'
 // }
-config.databaseId = ''
-config.containerId = ''
+config.databaseId = 'QuestionList'
+config.containerId = 'Items'
+config.appId = ''
+config.key = ''
+config.secret = ''
+config.cluster = ''
+
 module.exports = config
