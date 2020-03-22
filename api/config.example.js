8b7c65dd9ed5a8a3fbccf3005e07a17c92ec1034
Aviskar KC <aviskarkc10@gmail.com>
2020-03-22 23:59:04 +0545

    Deelete secrets from example config

--- api/config.example.js
+++ api/config.example.js
@@ -1,15 +1,8 @@
 var config = {}
-config.endpoint = 'https://ntozwu-ts-covid19-db.documents.azure.com:443/'
-config.key = '6o3oXBpwQN6God86QTXd67FoFa35jKmMPfhbxfHyuTsogQ6C46p75EC6e31jsKnujYztCZjpYC5PwBe2eC7ioA=='
-// config.database = {
-//   id: 'QuestionList'
-// }
-//
-// config.container = {
-//   id: 'DOCUMENTCOLLECTIONID'
-// }
-config.databaseId = 'QuestionList'
-config.containerId = 'Items'
+config.endpoint = ''
+config.key = ''
+config.databaseId = ''
+config.containerId = ''
 config.appId = ''
 config.pusherKey = ''
 config.secret = ''
