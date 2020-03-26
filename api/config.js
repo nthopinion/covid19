13b384cb2e67873e12d1117e14c08c1682fe49c8
nthopinion <62161323+nthopinion@users.noreply.github.com>
2020-03-25 17:58:00 -0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,18 +1,17 @@
 var config = {}
+config.endpoint = process.env["DBENDPOINT"]
+config.key = process.env["DBKEY"]
 
-config.endpoint = #{dbEndPoint}#
-config.key = #{dbEndPointKey}#
-
-config.databaseId = 'QuestionList'
-config.containerId = 'Items'
+config.databaseId = process.env["DBID"]
+config.containerId = process.env["CONTAINERID"]
 
 
 
 config.pusher = {
-    appId: '968198',
-    key: '99fd6f011f6f5a0d1565',
-    secret: '68a401ea7bf8764f36a9',
-    cluster: 'us3',
-    channel: 'my-channel'
+    appId: process.env["REACT_APP_CORS_PUSHER_ID"]
+    key: process.env["REACT_APP_CORS_PUSHER_KEY"]
+    secret: process.env["REACT_APP_CORS_PUSHER_SECRET"]
+    cluster: process.env["REACT_APP_CORS_PUSHER_CLUSTER"]
+    channel: process.env["REACT_APP_CORS_PUSHER_CHANNEL"]
 }
 module.exports = config
