07cb9b663da09d6487d63ae141e29d0aa39b9df3
milbz87 <alex.burn1987@gmail.com>
2020-04-08 17:33:09 -0700

    added db migration project to doNet folder

---
+++ dotNet/Covid19DbMigration/NewDataModel/Question.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Covid19DbMigration.NewDataModel
+{
+	public class Question: IQuestionAnswerItem
+	{
+		public Question()
+		{
+			Answers = new List<string>();
+		}
+
+		[JsonProperty(PropertyName = "id")]
+		public string Id { get; set; }
+		[JsonProperty(PropertyName = "date")]
+		public long Date { get; set; }
+		[JsonProperty(PropertyName = "title")]
+		public string Title { get; set; }
+		[JsonProperty(PropertyName = "answers")]
+		public List<string> Answers { get; set; }
+		[JsonProperty(PropertyName = "answered")]
+		public bool Answered { get; set; }
+		[JsonProperty(PropertyName = "like")]
+		public int Like { get; set; }
+	}
+}
