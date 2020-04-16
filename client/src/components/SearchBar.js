0c8b0fb8b7b6cce106b21773546a70759495a701
Binay <binaydhawa@outlook.com>
2020-04-16 20:43:44 +1000

    Search bar aligned

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 import React from 'react';
 import { Search } from 'semantic-ui-react';
 import { withTranslation } from 'react-i18next';
+// eslint-disable-next-line no-unused-vars
+import styles from '../styles/SearchBar.css';
 
 const SearchBar = (props) => {
   const { t } = props;
